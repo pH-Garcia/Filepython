fc10eab462b3fba4ec915ee24e59364f068a9641
paulo.garcia <ph.garcia.trdw@live.com>
2026-01-27 18:59:12 -0300

    arquivopy

---
+++ solodate.py
@@ -0,0 +1,13 @@
+nome = input ('Nome Completo:')
+cpf = input ('CPF:')
+rg = input  ('RG:')
+email = input ('Email:')
+
+print ('Bem Vindo(a)',nome,'!' , 'Siga para próxima etapa. ')
+
+idade = input ('Qual sua idade?')
+peso = input ('Qual seu peso?')
+altura = input ('Qual sua altura?')
+
+print ('Idade:',idade,'Peso:',peso,'Altura:',altura)
+
